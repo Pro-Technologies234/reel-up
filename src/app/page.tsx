98c20b61610c7c5b553f0cbbd8624c8075fb5fa0
Pro-Technologies234 <protechnologies332@gmail.com>
2025-07-23 13:48:06 +0100

    removed unused data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { Navbar } from "../components/shared/navbar";
 import { Hero } from "@/components/home/Hero";
 import { SellLikePro } from "@/components/home/SellLikePro";
